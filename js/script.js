5040a6f6d566feeaffbf96fe3fa0acb7b7ca10bd
barabaaara <htht.bara@gmail.com>
2021-01-02 17:00:45 +0900

    Workのモーダルウィンドウ処理追加

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@
 function getAge() {
     var age = 0;
     var today = new Date();
-    var date1 = new Date(today.getFullYear(), 3, 8, 0, 0, 0);
+    var date1 = new Date(today.getFullYear(), 2, 8, 0, 0, 0);
     var birthYear = 1993;
 
-    if (today > date1) {
+    if (today >= date1) {
         age = today.getFullYear() - birthYear;
     } else {
-        age = today.getFullYear() - birthYear + 1;
+        age = today.getFullYear() - birthYear - 1;
     }
 
     return age;
